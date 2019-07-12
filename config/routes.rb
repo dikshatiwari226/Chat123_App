68b648b0875cde84079e48e1d1a8231bcd8b6ceb
arvind kushwah <arvindkushwah9@gmail.com>
2019-07-12 12:19:17 +0530

    add

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,13 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   root 'home#index'
+
+
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+    resources :messages, only: [:create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
